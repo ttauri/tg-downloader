855b99e1b6598f78ae5c246a52737777b79cc161
ceti <owl.tranquil@gmail.com>
2024-07-30 21:50:50 +0300

    fix sorting type var

--- app/services/periodic_task.py
+++ app/services/periodic_task.py
@@ -40,7 +40,8 @@ async def check_for_new_messages():
 
 async def download_media_from_channel(channel_id: int):
     db = SessionLocal()
-    sorting_type = config.
+    sorting_type = config.sorting_type
+    logger.info(f"Using {sorting_type} sorting")
     media = get_all_not_downloaded_media(db, channel_id, order="large")
     async with client:
         for m in media:
